06ce1939f7538bcc5e32c3d27504e5a5a3dc21e8
Caitlin Mothes <ccmothes@gmail.com>
2025-01-23 17:02:05 -0700

    attempt to change leaflet native CRS

--- data_processing.R
+++ data_processing.R
@@ -1,11 +1,12 @@
 # Code to process and test large rasters
 
 source("setup.R")
-install.packages("gdalUtilities")
+library(gdalUtilities)
 
 
 hfi_1999 <- terra::rast("L:\\Projects_active\\2024_x2025_Human_Footprint_Index\\data\\1999_int16.tif")
 
+# this will take a while...
 hfi_1999_projected <- terra::project(hfi_1999, "EPSG:3857")
 
 hfi_2023 <- terra::rast("L:\\Projects_active\\2024_x2025_Human_Footprint_Index\\data\\2023_int16.tif")
@@ -14,11 +15,12 @@ hfi_change <- terra::rast("L:\\Projects_active\\2024_x2025_Human_Footprint_Index
 
 
 # attempt plotting (takes so long due to projection)
-leaflet() %>%
+leaflet(options = leafletOptions(crs = leafletCRS(crsClass = "L.CRS.EPSG4326"))) %>%
 addProviderTiles("OpenStreetMap") %>%
-  addRasterImage(hfi_1999, colors = terrain.colors(100), opacity = 0.8)
+  addRasterImage(hfi_1999, colors = terrain.colors(100), opacity = 0.8, project = FALSE)
 
 
+# Attemp to create tiles...may need to install GDAL
 # Define the input file and output directory
 #input_raster <- "path_to_your_large_raster.tif"  # Replace with your raster file path
 output_dir <- "data/"  # Directory to store the output tiles
